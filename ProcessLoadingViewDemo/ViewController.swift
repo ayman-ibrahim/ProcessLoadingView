b8f362010229bda11a5efaed45ab2aadc99ff06d
ayman.ibrahim <ayman.ibrahim.alim@gmail.com>
2018-01-12 16:31:00 +0300

    Initial Commit

---
+++ ProcessLoadingViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProcessLoadingViewDemo
+//
+//  Created by Ayman Ibrahim on 1/12/18.
+//  Copyright © 2018 Ayman Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
